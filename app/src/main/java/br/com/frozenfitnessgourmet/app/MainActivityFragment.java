e66d3c6b14879e1dad4ff6eb6c772437d22217e4
AMANDA RAFAELA BARRETO DE MELO <15160046@CFP127ALUNO.LOCAL>
2016-09-26 14:17:08 -0300

    first commit

---
+++ app/src/main/java/br/com/frozenfitnessgourmet/app/MainActivityFragment.java
@@ -0,0 +1,43 @@
+package br.com.frozenfitnessgourmet.app;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        Button btnentrar = (Button) view.findViewById(R.id.btnentrar);
+
+        btnentrar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CenterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+        return view;
+    }
+}
